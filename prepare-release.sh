7370354340315eaa5bd6a25367135548f39f776c
Mathieu Carbou <mathieu.carbou@gmail.com>
2023-05-04 20:46:10 +0200

    custom build

--- prepare-release.sh
+++ prepare-release.sh
@@ -2,12 +2,13 @@
 
 # Script to compile all released enviroments and copy build artifacts to project root for easy upload to github releases. 
 
-pio run -e emonevse
-pio run -e openevse
-pio run -e emonevse_eu
+#pio run -e emonevse
+#pio run -e openevse
+#pio run -e emonevse_eu
+pio run -e openevse_eu
 
-cp .pio/build/emonevse/firmware.hex emonevse.hex
-cp .pio/build/openevse/firmware.hex openevse.hex
+#cp .pio/build/emonevse/firmware.hex emonevse.hex
+#cp .pio/build/openevse/firmware.hex openevse.hex
 cp .pio/build/openevse_eu/firmware.hex openevse_eu.hex
 ls *.hex
 
